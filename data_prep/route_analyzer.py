3de46b30d73b0f090817d1e3c0b36bace8b9f4e5
giogvn <giovanitavares@outlook.com>
2023-02-25 22:27:54 -0300

    Starts analyzing the data
    Signed-off-by: giogvn <giovanitavares@outlook.com>

--- data_prep/route_analyzer.py
+++ data_prep/route_analyzer.py
@@ -1,48 +1,8 @@
+import json, os
 import pandas as pd
 from geopy import distance
-import json, os
 from datetime import datetime
-
-
-class DataFrameCreator:
-    def __init__(self, routes_dir: str):
-        self.routes_dir = routes_dir
-        self.routes, self.routes_info = self.read_routes_info(routes_dir)
-
-    def read_route_from_json(
-        self, json_path: str, route_key: str = "route", info_key: str = "general_info"
-    ) -> list:
-        with open(json_path, "r") as f:
-            data = json.load(f)
-            route = data[route_key]
-            route_info = data[info_key]
-        return route, route_info
-
-    def add_route_id_to_points(self, route_id: int, route: list) -> None:
-        for point in route:
-            point["route_id"] = route_id
-
-    def read_routes_info(self, routes_dir: str) -> list:
-        routes = []
-        routes_info = {}
-        route_id = 0
-        for path in os.listdir(self.routes_dir):
-            route_path = os.path.join(self.routes_dir, path)
-            if os.path.isfile(route_path):
-                route, route_info = self.read_route_from_json(route_path)
-                self.add_route_id_to_points(route_id, route)
-                routes_info[route_id] = route_info
-                routes.append(route)
-                route_id += 1
-        return routes, routes_info
-
-    def create_routes_dataframe(self) -> pd.DataFrame:
-        routes_df = pd.DataFrame()
-        for route in self.routes:
-            df = pd.DataFrame.from_records(route)
-            routes_df = pd.concat([routes_df, df])
-
-        return routes_df
+from dataframe_creator import DataFrameCreator
 
 
 class RouteAnalyzer:
@@ -52,10 +12,8 @@ class RouteAnalyzer:
     def get_distance_between_coords(self, coord1: tuple, coord2: tuple) -> float:
         return distance.distance(coord1, coord2).meters
 
-    def calculate_route_total_dist_and_time(self, route_id: int = 0) -> list:
+    def calculate_route_dists_and_times(self, route_id: str) -> list:
         distances = [(0, 0)]
-        total_dist = 0
-        total_time = 0
         route = self.routes_df.loc[self.routes_df["route_id"] == route_id]
 
         for index, row in route.iterrows():
@@ -74,18 +32,27 @@ class RouteAnalyzer:
             distance = self.get_distance_between_coords(coord1, coord2)
             delta_t = (time2 - time1).total_seconds()
 
-            total_dist += distance
-            total_time += delta_t
-            distances.append((total_dist, total_time))
+            distances.append((distance, delta_t))
         return distances
 
+    def calculate_route_total_dists_and_times(self, route_id: str) -> tuple:
+        points = self.calculate_route_dists_and_times(route_id)
+        dists = [p[0] for p in points]
+        times = [p[1] for p in points]
 
-if __name__ == "__main__":
-    routes_dir = "../data/json_data/"
-    df_creator = DataFrameCreator(routes_dir)
-    df = df_creator.create_routes_dataframe()
-    route_analyzer = RouteAnalyzer(df)
-    calc_diff = []
-    for route_id in range(df["route_id"].max()):
-        total_route = route_analyzer.calculate_route_total_dist_and_time(route_id)
-        break
+        acumulated_dists = [sum(dists[: i + 1]) for i in range(len(dists))]
+        acumulated_times = [sum(times[: i + 1]) for i in range(len(times))]
+
+        return acumulated_dists, acumulated_times
+
+    def calculate_speeds_and_times(self, route_id: str) -> list:
+        route = self.calculate_route_dists_and_times(route_id)
+        speeds = [0]
+        total_t = 0
+        times = [total_t]
+        for index, point in enumerate(route[1:]):
+            speeds.append(point[0] / point[1])
+            total_t += point[1]
+            times.append(total_t)
+
+        return speeds, times
